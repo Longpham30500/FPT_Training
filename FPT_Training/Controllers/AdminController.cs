c81b5e629d7760b04671baf94288d5bc0cc96b94
thien tran <trannguyenduythien@outlook.com.vn>
2021-11-01 22:39:05 +0700

    Create Trainer Model, Admin Controller and Ultils for default value of Roles

---
+++ FPT_Training/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using FPT_Training.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPT_Training.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+        private ApplicationUserManager _userManager;
+        public AdminController()
+        {
+            UserManager = _userManager;
+            _context = new ApplicationDbContext();
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FPT_Training/Migrations/202111011536339_Check.cs
@@ -0,0 +1,31 @@
+namespace FPT_Training.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Check : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.AspNetUsers", "Specialty", c => c.String(maxLength: 50));
+            AddColumn("dbo.AspNetUsers", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Discriminator");
+            DropColumn("dbo.AspNetUsers", "Specialty");
+            DropTable("dbo.Categories");
+        }
+    }
+}
