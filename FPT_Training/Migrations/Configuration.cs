b6b5ab500117a1b93b8f6b97f67edb871bafbbbc
Long Phạm <Longpham30500@gmail.com>
2021-11-01 18:31:26 +0700

    Create table Category

---
+++ FPT_Training/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FPT_Training.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FPT_Training.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FPT_Training.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
