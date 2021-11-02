de5553f7e732079ddc771d3e22d6440bc8631fb8
Long Phạm <Longpham30500@gmail.com>
2021-11-02 14:54:37 +0700

    Create Action,view,view models for Create Course in Course Controller(finish course)

--- FPT_Training/Controllers/CourseController.cs
+++ FPT_Training/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using FPT_Training.Models;
+using FPT_Training.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,24 +8,53 @@ using System.Web.Mvc;
 
 namespace FPT_Training.Controllers
 {
-    public class CourseController : Controller
-    {
-        // GET: Course
-        private ApplicationDbContext _context;
-        public CourseController()
-        {
-            _context = new ApplicationDbContext();
-        }
+	public class CourseController : Controller
+	{
+		// GET: Course
+		private ApplicationDbContext _context;
+		public CourseController()
+		{
+			_context = new ApplicationDbContext();
+		}
 
-        public ActionResult Index(string Search)
-        {
-            IEnumerable<Course> courses = _context.Courses;
-            if (!String.IsNullOrEmpty(Search))
-            {
-                Search = Search.ToLower();
-                courses = courses.Where(x => x.CourseName.ToLower().Contains(Search));
-            }
-            return View(courses.ToList());
-        }
-    }
+		public ActionResult Index(string Search)
+		{
+			IEnumerable<Course> courses = _context.Courses;
+			if (!String.IsNullOrEmpty(Search))
+			{
+				Search = Search.ToLower();
+				courses = courses.Where(x => x.CourseName.ToLower().Contains(Search));
+			}
+			return View(courses.ToList());
+		}
+
+		public ActionResult CreateCourse()
+		{
+			var newView = new CourseCategoriesModel
+			{
+				categories = _context.Categories.ToList()
+			};
+			return View(newView);
+		}
+
+		[HttpPost]
+		public ActionResult CreateCourse(CourseCategoriesModel courseCategories)
+		{
+			if (ModelState.IsValid)
+			{
+				var isExisted = _context.Courses.SingleOrDefault(m => m.CourseName == courseCategories.course.CourseName);
+				if (isExisted != null)
+					return View();
+				var newCourse = new Course()
+				{
+					CourseName = courseCategories.course.CourseName,
+					Description = courseCategories.course.Description,
+					CategoryId = courseCategories.Id,
+				};
+				var data = _context.Courses.Add(newCourse);
+			}
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ FPT_Training/ViewModels/CourseCategoriesModel.cs
@@ -0,0 +1,15 @@
+﻿using FPT_Training.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FPT_Training.ViewModels
+{
+	public class CourseCategoriesModel
+	{
+		public Course course { get; set; }
+		public int Id { get; set; }
+		public List<Category> categories { get; set; }
+	}
+}
