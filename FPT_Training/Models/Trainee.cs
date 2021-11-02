e6979c02e31f772d20315f96f4e86d0e2f04646d
Long Phạm <Longpham30500@gmail.com>
2021-11-02 15:14:14 +0700

    Create model for trainee, StaffController and add-migration for trainee

---
+++ FPT_Training/Models/Trainee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FPT_Training.Models
+{
+	public class Trainee
+	{
+		[MaxLength(50)]
+		public string Education { get; set; }
+	}
+}
