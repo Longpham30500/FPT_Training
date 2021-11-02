c24f2cebfee1db9d8cec690d06e643dfcd3f941e
Long Phạm <Longpham30500@gmail.com>
2021-11-02 14:39:19 +0700

    Create action, view Index of Course(finish course)

---
+++ FPT_Training/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using FPT_Training.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPT_Training.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        private ApplicationDbContext _context;
+        public CourseController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index(string Search)
+        {
+            IEnumerable<Course> courses = _context.Courses;
+            if (!String.IsNullOrEmpty(Search))
+            {
+                Search = Search.ToLower();
+                courses = courses.Where(x => x.CourseName.ToLower().Contains(Search));
+            }
+            return View(courses.ToList());
+        }
+    }
+}
