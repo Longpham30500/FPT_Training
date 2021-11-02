08004407ec1a625ce8b12d895d6e5271e591b16b
Long Phạm <Longpham30500@gmail.com>
2021-11-02 14:22:47 +0700

    Create model Course

---
+++ FPT_Training/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FPT_Training.Models
+{
+	public class Course
+	{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(255)]
+    [Required]
+    public string CourseName { get; set; }
+
+    [StringLength(255)]
+    [Required]
+    public string Description { get; set; }
+
+    [ForeignKey("Category")]
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+  }
+}
